e6d8d3f1f64fefdd52fe402c7369e8d54b93141e
rafaa1999 <raferhouma10@gmail.com>
2024-02-15 15:53:40 +0100

    Dynamic Configuration of routes

--- gateway-service/src/main/java/com/rafaa/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/rafaa/GatewayServiceApplication.java
@@ -3,10 +3,19 @@ package com.rafaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class);
     }
+
+   @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+       return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+   }
 }
