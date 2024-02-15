1c6107049dc10c875bcda72c334f8259e46301e2
rafaa1999 <raferhouma10@gmail.com>
2024-02-15 10:19:56 +0100

    Setup services:   - discovery-service   - gateway-service   - config-service   - consumer-service   - account-service

---
+++ customer-service/src/main/java/com/rafaa/CustomerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.rafaa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+    public static void main(String[] args) {
+       SpringApplication.run(CustomerServiceApplication.class);
+    }
+}
---
+++ gateway-service/src/main/java/com/rafaa/GatewayServiceApplication.java
@@ -0,0 +1,10 @@
+package com.rafaa;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class);
+    }
+}
