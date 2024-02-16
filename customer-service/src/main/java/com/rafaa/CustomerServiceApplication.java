d0f60d5a3841361d85a809d04ae7978b2216a22c
rafaa1999 <raferhouma10@gmail.com>
2024-02-16 19:48:38 +0100

    change

--- customer-service/src/main/java/com/rafaa/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/rafaa/CustomerServiceApplication.java
@@ -1,15 +1,19 @@
 package com.rafaa;
 
+import com.rafaa.config.GlobalConfig;
 import com.rafaa.entities.Customer;
 import com.rafaa.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
 @SpringBootApplication
+// Inject the configuration from the config-service
+@EnableConfigurationProperties(GlobalConfig.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
--- customer-service/src/main/java/com/rafaa/web/ConfigTestController.java
+++ customer-service/src/main/java/com/rafaa/web/ConfigTestController.java
@@ -1,12 +1,16 @@
 package com.rafaa.web;
 
+import com.rafaa.config.GlobalConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class ConfigTestController {
    @Value("${global.params.p1}")
    private int p1;
@@ -16,8 +20,14 @@ public class ConfigTestController {
 //   private int x;
 //   @Value("${global.params.y}")
 //   private int y;
-   @RequestMapping("/testConfig")
+   @Autowired
+   private GlobalConfig globalConfig;
+   @GetMapping("/testConfig")
    public Map<String,Integer> configTest(){
        return Map.of("p1", p1, "p2", p2);
+   }
+   @GetMapping("/globalConfig")
+   public GlobalConfig globalConfig() {
+      return globalConfig;
    }
 }
